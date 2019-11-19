01b1eb3de1afb3aa3dea83048e781470adb8273d
Sebastian Pekarek <mail@sebbo.net>
2019-11-19 21:38:16 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# atv2mqtt
+
+[![License](https://img.shields.io/badge/license-MIT-blue.svg?style=flat-square)](LICENSE)
+
+AppleTV2MQTT allows you to remote control your Apple TV using the MQTT protocol. Many home automation systems support 
+this protocol, so Apple TV can be integrated into your existing automation system. In addition to 
+simulating key presses, you can also query the current state of Apple TV. This module uses a forked version of 
+[node-appletv](https://github.com/evandcoleman/node-appletv).
+
+
+## ☁ Installation
+
+To install the javascript module via npm run:
+
+	npm install -g http://github.com/sebbo2002/atv2mqtt.git
+
+
+## ⚒ Quick Start
+
+1. Use node-appletv to connect to your Apple TV and authenticate [[?](https://github.com/evandcoleman/node-appletv#as-a-standalone-cli)]
+
+2. Create a new atv2mqtt configuration file
+```json
+{
+  "broker": "mqtt://192.168.1.1",
+  "devices": [
+    {
+      "topic": "/home/livingroom/appletv",
+      "credentials": "************************************"
+    }
+  ]
+}
+```
+
+3. Start atv2mqtt
+```bash
+atv2mqtt /home/eve/atv2mqtt.json
+```
+
+
+
+## Copyright and license
+
+Copyright (c) Sebastian Pekarek under the [MIT license](LICENSE).
---
+++ src/index.js
@@ -0,0 +1,170 @@
+'use strict';
+
+const mqtt = require('mqtt');
+const AppleTV = require('node-appletv');
+
+const keys = [
+    'up',
+    'down',
+    'left',
+    'right',
+    'menu',
+    'play',
+    'pause',
+    'next',
+    'previous',
+    'suspend',
+    'select',
+    'LongTv',
+    'Tv'
+];
+
+/**
+ * @author Sebastian Pekarek
+ * @module @sebbo2002/atv2mqtt
+ * @class ATV2MQTT
+ */
+class ATV2MQTT {
+    constructor(options) {
+        if (typeof options.broker !== 'string') {
+            throw new Error('options.broker is not set!');
+        }
+        if (!Array.isArray(options.devices) || !options.devices.length) {
+            throw new Error('options.devices is not set!');
+        }
+        if (options.devices.find(d => typeof d.topic !== 'string')) {
+            throw new Error('options.devices.topic is not set!');
+        }
+        if (options.devices.find(d => typeof d.credentials !== 'string')) {
+            throw new Error('options.devices.credentials is not set!');
+        }
+
+        this._options = options;
+        this._teardown = [];
+
+        this
+            ._start()
+            .catch(err => {
+                this._log('error', null, 'Unable to start', err);
+            });
+    }
+
+    _log(level, id, message, error) {
+        if (this._options.log) {
+            try {
+                this._options.log.apply(this, [level, id, message, error]);
+            } catch (err) {
+                console.log('Unable to call custom log function:');
+                console.log(err);
+            }
+        }
+    }
+
+    async _start() {
+        this._mqtt = mqtt.connect(this._options.broker);
+        this._mqtt.on('error', error => this._log('error', null, null, error));
+        this._teardown.unshift(() => {
+            this._mqtt.off('error');
+            return new Promise(resolve => {
+                this._mqtt.end(false, () => {
+                    resolve();
+                });
+            });
+        });
+
+        await Promise.all(
+            this._options.devices.map(
+                device => this._startDevice(device)
+            )
+        );
+    }
+
+    async _startDevice(device) {
+        const id = this._options.devices.indexOf(device) + 1;
+        this._log('info', id, 'Search for device');
+
+        const credentials = AppleTV.parseCredentials(device.credentials);
+        const devices = await AppleTV.scan(credentials.uniqueIdentifier);
+        this._log('info', id, `Found ${devices.length} devices`);
+        if(!devices.length) {
+            this._log('info', id, `Device not found, try again in a bit…`);
+            await new Promise(cb => setTimeout(cb, 30000));
+            return this._startDevice(device);
+        }
+
+        let push, shutdownPush;
+        const appletv = await devices[0].openConnection(credentials);
+
+
+        /* MQTT <--  PYATV */
+
+        this._mqtt.publish(device.topic + '/name', appletv.name, {retain: true});
+        this._mqtt.publish(device.topic + '/address', appletv.address, {retain: true});
+
+        this._log('info', id, 'Open push connection');
+        appletv.on('nowPlaying', info => this.transmitCurrentPlaying(id, device, info));
+
+        appletv.on('error', error => {
+            this._log('error', id, 'Error', error);
+        });
+        appletv.on('close', () => {
+            this._log('info', id, 'Connection closed');
+            if (!shutdownPush) {
+                this._startDevice(device);
+            }
+        });
+
+        this._teardown.unshift(() => {
+            shutdownPush = true;
+            appletv.closeConnection();
+        });
+
+
+        /* MQTT -->  PYATV */
+
+        this._mqtt.subscribe(device.topic + '/+');
+        this._teardown.unshift(() => {
+            return new Promise(resolve => this._mqtt.unsubscribe(device.topic + '/+', resolve));
+        });
+
+        this._mqtt.on('message', topic => {
+            const key = keys.find(key => device.topic + '/' + key === topic);
+            if (key && AppleTV.AppleTV.key(key)) {
+                appletv.sendKeyCommand(AppleTV.AppleTV.key(key)).catch(err => {
+                    this._log('error', id, `Unable to press key "${key}"`, err);
+                });
+            }
+        });
+    }
+
+    transmitCurrentPlaying (id, device, info) {
+        [
+            'duration',
+            'elapsedTime',
+            'title',
+            'artist',
+            'album',
+            'appDisplayName',
+            'appBundleIdentifier',
+            'playbackState',
+            'timestamp'
+        ].forEach(attribute => {
+            let value = '';
+            if(info && typeof info[attribute] === 'string') {
+                value = info[attribute];
+            }
+            else if(info && typeof info[attribute] === 'object') {
+                value = JSON.stringify(info[attribute]);
+            }
+
+            this._mqtt.publish(device.topic + '/' + attribute, value, {retain: true});
+        });
+    }
+
+    async stop() {
+        await Promise.all(this._teardown);
+    }
+}
+
+
+module.exports = ATV2MQTT;
